78f12ea90ca5a5f831e2a84a18701fc75f9b412e
Shasank Shrestha <sasan.stha@gmail.com>
2022-10-12 13:55:03 +0545

    Folder created and 4 task completed

---
+++ Week1/que3.py
@@ -0,0 +1,3 @@
+cel = 38.4
+fah = (cel * 9/5) + 32
+print("Temperature in Yorkshire",fah)
---
+++ Week1/que4.py
@@ -0,0 +1,6 @@
+matches_pl = 609
+batted = 1014
+not_out = 162
+runs = 48426
+bt_av = runs / (batted - not_out)
+print("Batting Average =", bt_av)
