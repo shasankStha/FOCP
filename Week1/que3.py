0f9784893f1949e6da691b041ef6fc3557f66c31
Shasank Shrestha <sasan.stha@gmail.com>
2022-10-13 18:04:41 +0545

    Exercise file added

--- Week1/que3.py
+++
@@ -1,3 +0,0 @@
-cel = 38.4
-fah = (cel * 9/5) + 32
-print("Temperature in Yorkshire is",fah)
--- Week1/que4.py
+++
@@ -1,6 +0,0 @@
-matches_pl = 609
-batted = 1014
-not_out = 162
-runs = 48426
-bt_av = runs / (batted - not_out)
-print("Batting Average =", bt_av)
