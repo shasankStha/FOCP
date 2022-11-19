cdc626b1d3028c614a49f6efdf03c54bed8a3e1d
Shasank Shrestha <sasan.stha@gmail.com>
2022-11-19 13:33:01 +0545

    first_prog.py

---
+++ Week5/first_prog.py
@@ -0,0 +1,14 @@
+# taking input from user and storing it in number
+number = input("Enter a number: ")
+# converting type of data to integer
+number = int(number)
+# printing the entered number
+print("The numbered entered was", number)
+# checking whether the number is odd or even. If condition meets, if will execute, if not else will execute.
+if (number % 2) == 0:
+    if (number %10) == 0:
+	    print("That is an even number and divisible by 10")
+    else:
+        print("That is an even number")
+else:
+	print("That is an odd number")
